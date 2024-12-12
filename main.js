c4ba9acf28d4b1219daa9aec73ab90cab5d9fd6d
Kim Lida <88290393+Kinukeo@users.noreply.github.com>
2024-12-12 19:04:31 +0000

    removed fetch cache added html and styling

--- main.js
+++ main.js
@@ -1,36 +1,33 @@
-// grabs the click button 
+// grabs the click button
 const clickHereButton = document.getElementById("clickButton");
 
 // button says when clicked to grab the adviceText from api
-clickHereButton.addEventListener("click",getAndDisplayFortune);
-
+clickHereButton.addEventListener("click", getAndDisplayFortune);
 
 // function to display the advice from the fetch request function [retrieveFortune]
 async function getAndDisplayFortune() {
-    const fortune = await retrieveFortune();
-    displayFortune(fortune);
-} 
+  const fortune = await retrieveFortune();
+  displayFortune(fortune);
+}
 
 // 'fetch advice from API' function
 async function retrieveFortune() {
-    const response = await fetch("https://api.adviceslip.com/advice",{
-        
-});
+  try {
+    const response = await fetch("https://api.adviceslip.com/advice", {
+      cache: "no-store",
+    });
     const json = await response.json();
     console.log(json.slip.advice);
-    return json;
+    return json.slip.advice;
+  } catch {
+    console.log("API Request Unsuccessful");
+  }
 }
 
 // grabbing the <p> element for where the advice will go on the web page
 
 function displayFortune(fortune) {
-    const adviceText = document.getElementById("adviceText");
-    adviceText.textContent = fortune.slip.advice
+  const adviceText = document.getElementById("adviceText");
+  adviceText.textContent = fortune;
 }
-retrieveFortune()
-
-// async function log() {
-//     console.log(retriveFortune.advice)
-// }
-
-//console.log("hiii")
+retrieveFortune();
