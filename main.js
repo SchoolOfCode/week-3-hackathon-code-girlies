0563be3001813ead51a99a8b881e70a40d011157
CarmenZak <186872558+CarmenZak@users.noreply.github.com>
2024-12-12 15:53:52 +0000

    Add html button/text/placeholder and js api function and event listener

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
+// grabs the click button 
+const clickHereButton = document.getElementById("clickButton");
 
-async function retriveFortune() {
+// button says when clicked to grab the adviceText from api
+clickHereButton.addEventListener("click",getAndDisplayFortune);
+
+
+// function to display the advice from the fetch request function [retrieveFortune]
+async function getAndDisplayFortune() {
+    const fortune = await retrieveFortune();
+    displayFortune(fortune);
+} 
+
+// 'fetch advice from API' function
+async function retrieveFortune() {
     const response = await fetch("https://api.adviceslip.com/advice",{
         
 });
@@ -8,7 +21,13 @@ async function retriveFortune() {
     return json;
 }
 
-retriveFortune()
+// grabbing the <p> element for where the advice will go on the web page
+
+function displayFortune(fortune) {
+    const adviceText = document.getElementById("adviceText");
+    adviceText.textContent = fortune.slip.advice
+}
+retrieveFortune()
 
 // async function log() {
 //     console.log(retriveFortune.advice)
